296ffed1e2611b39abb04eaa9fcdaeda3f536801
Patrik Suzzi <psuzzi@gmail.com>
2020-05-17 12:48:51 +0200

    Add initial solution for sliding window

---
+++ prepare/s1/src/w2/p05/Solution.java
@@ -0,0 +1,20 @@
+package w2.p05;
+
+public class Solution {
+	
+	public static void main(String[] args) {
+		System.out.printf("%s - %s", (int)'Z', (int)'A');
+	}
+
+    public int characterReplacement(String s, int k) {
+    	char[] chars = s.toCharArray();    	
+    	int[] count = new int[26];
+    	int maxCount = 0;
+    	int l=0, r=chars.length-1;
+    	while( l<r ) {
+    		
+    	}
+        return max;
+    }
+
+}
