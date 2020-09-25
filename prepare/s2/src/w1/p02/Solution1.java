da5dd0fac01764e4ff15afd5156e598a145a1819
Patrik Suzzi <psuzzi@gmail.com>
2020-09-25 19:44:11 +0200

    add w1 p02

---
+++ prepare/s2/src/w1/p02/Solution1.java
@@ -0,0 +1,42 @@
+package w1.p02;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        
+        if(l1==null && l2==null)
+            return null;
+        
+        ListNode parent = null, root = null;
+        while(l1!=null || l2!=null){
+            ListNode curr;
+            if(l1==null || l2!=null && l2.val<l1.val){
+                curr = l2;
+                l2 = l2.next;
+            } else {
+                // l2 is null or l1.val<=l2.val
+                curr = l1;
+                l1=l1.next;
+            }
+            if(parent==null){
+                root=curr;
+            } else {
+                // update current parent
+                parent.next=curr;
+            }
+            // next parent
+            parent=curr;
+        }
+        return root;
+    }
+
+}
