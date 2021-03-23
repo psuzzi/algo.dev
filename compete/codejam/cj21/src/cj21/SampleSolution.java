3aad034acd07c9619cfd4ca0237c3f38c142c810
Patrik Suzzi <patrik.suzzi@adnovum.ch>
2021-03-23 07:06:47 +0100

    add cj 21

---
+++ compete/codejam/cj21/src/cj21/SampleSolution.java
@@ -0,0 +1,21 @@
+package cj21;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class SampleSolution {
+	
+	static PrintStream out = System.out;
+
+	public static void main(String[] args) {
+		
+		try(Scanner in = new Scanner(System.in)){
+			int t = in.nextInt();
+			for( int it = 1; it<=t; it++ ) {
+				out.printf("Case #%s%n", it);
+			}
+		}
+
+	}
+
+}
